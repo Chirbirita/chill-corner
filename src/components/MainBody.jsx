deeea339c1978c19616d7697599f3dfa9fe0305f
Drzazga88 <drzewiecka.katarzyna88@gmail.com>
2023-03-22 20:40:53 +0000

    added main background image and added navbar to the main container

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,7 +1,32 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light p-4" style={{backgroundColor: "#A1B89B", borderRadius: '8px', display: 'flex', alignItems: 'center'}}>
+    <a href="/" className="text-white font-bold ml-4">Home</a>
+    <div style={{display: 'flex', alignItems: 'center', marginLeft: '23em'}}>
+      <a href="/about" className="text-white ml-12">About</a>
+      <a href="/contact" className="text-white ml-4">Contact</a>
+      <a href="/placeholder" className="text-white ml-4">Placeholder</a>
+    </div>
+  </nav>
+  );
+};
+
 const MainBody = () => {
     return (
-        <div className="glass w-4/6 h-5/6 my-auto rounded-lg">
+        <>
+       
+        <div className="glass w-full h-5/6 my-auto rounded-lg"
+        style={{
+            backgroundImage: `url(./images/chill-out_copy.jpeg)`,
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center',
+          }}>
+             <Navbar />
         </div>
+        
+        </>
     )
 
 }
