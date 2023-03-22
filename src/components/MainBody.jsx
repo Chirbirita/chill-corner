9e87b083be7ab8204b7e7d71ffd1d9f702fa9cfd
Drzazga88 <drzewiecka.katarzyna88@gmail.com>
2023-03-22 20:51:00 +0000

    added fade effect to the navbar

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -2,32 +2,49 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-light p-4" style={{backgroundColor: "#A1B89B", borderRadius: '8px', display: 'flex', alignItems: 'center'}}>
-    <a href="/" className="text-white font-bold ml-4">Home</a>
-    <div style={{display: 'flex', alignItems: 'center', marginLeft: '23em'}}>
-      <a href="/about" className="text-white ml-12">About</a>
-      <a href="/contact" className="text-white ml-4">Contact</a>
-      <a href="/placeholder" className="text-white ml-4">Placeholder</a>
-    </div>
-  </nav>
+    <nav
+      className="navbar navbar-light p-4"
+      style={{
+        backgroundColor: 'rgba(161,184,155,0.75)',
+        borderRadius: '8px',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <a href="/" className="ml-4 font-bold text-white">
+        Home
+      </a>
+      <div
+        style={{ display: 'flex', alignItems: 'center', marginLeft: '23em' }}
+      >
+        <a href="/about" className="ml-12 text-white">
+          About
+        </a>
+        <a href="/contact" className="ml-4 text-white">
+          Contact
+        </a>
+        <a href="/placeholder" className="ml-4 text-white">
+          Placeholder
+        </a>
+      </div>
+    </nav>
   );
 };
 
 const MainBody = () => {
-    return (
-        <>
-       
-        <div className="glass w-full h-5/6 my-auto rounded-lg"
+  return (
+    <>
+      <div
+        className="glass my-auto h-5/6 w-full rounded-lg"
         style={{
-            backgroundImage: `url(./images/chill-out_copy.jpeg)`,
-            backgroundSize: 'cover', 
-            backgroundPosition: 'center',
-          }}>
-             <Navbar />
-        </div>
-        
-        </>
-    )
-
-}
-export default MainBody
+          backgroundImage: `url(./images/chill-out_copy.jpeg)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <Navbar />
+      </div>
+    </>
+  );
+};
+export default MainBody;
