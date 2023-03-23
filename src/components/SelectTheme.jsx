8da69cc44af73118180f599d0a9f2d5ed0021244
Drzazga88 <drzewiecka.katarzyna88@gmail.com>
2023-03-23 22:56:49 +0000

    added side bar buttons for mood and time selection together with start button

--- src/components/SelectTheme.jsx
+++ src/components/SelectTheme.jsx
@@ -7,7 +7,7 @@ const SelectTheme = () => {
         <form className=' bg-[rgb(187,220,163,0.6)] w-full p-7 rounded-md'>
             <label htmlFor="theme">
                <h1 className="ChooseMe mb-3" style={{color: 'white', fontFamily: 'Inter, sans-serif'}}>
-                CHOOSE A THEME</h1>
+                What is your mood today?</h1>
                 <select id='theme' value={theme} placeholder="Choose Theme" className="mb-5 block w-60" onChange={(e) => setTheme(e.target.value)}>
                     <option /> {/** this is to add an empty line to the options list */}
                     {/* {THEMES.map((theme) => (
