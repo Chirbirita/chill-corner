94987e3b00c5ac71a3f4f5fb0efdf44f277f5c04
Drzazga88 <drzewiecka.katarzyna88@gmail.com>
2023-03-23 21:37:09 +0000

    added notes section to main body

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaPlay, FaPause, FaStepForward, FaStepBackward } from 'react-icons/fa';
 
+// NavBar
 const Navbar = () => {
   return (
     <nav
@@ -32,6 +33,7 @@ const Navbar = () => {
   );
 };
 
+// Our App name; this can be moved to side bar later if needed
 const TextBox = () => {
   return (
     <div
@@ -58,6 +60,7 @@ const TextBox = () => {
   );
 };
 
+// Music player but no particular logic yet
 const MusicPlayer = () => {
   const [playing, setPlaying] = useState(false);
 
@@ -73,21 +76,94 @@ const MusicPlayer = () => {
     // TODO: logic for going to the next song
   };
 
-
   return (
     <div className="mt-8 flex items-center justify-center">
-        <div className="flex gap-6">
-        <FaStepBackward size={50} color="#FFF" onClick={handlePrevious} style={{marginTop: 5, marginBottom: 5}}/>
-      {playing ? (
-        <FaPause size={70} color="#FFF" onClick={handleClick} />
-      ) : (
-        <FaPlay size={70} color="#FFF" onClick={handleClick} />
-      )}
-      <FaStepForward size={50} color="#FFF" onClick={handleNext} style={{marginTop: 5, marginBottom: 5}}/>
-    </div>
+      <div className="flex gap-6">
+        <FaStepBackward
+          size={50}
+          color="#FFF"
+          onClick={handlePrevious}
+          style={{ marginTop: 5, marginBottom: 5 }}
+        />
+        {playing ? (
+          <FaPause size={70} color="#FFF" onClick={handleClick} />
+        ) : (
+          <FaPlay size={70} color="#FFF" onClick={handleClick} />
+        )}
+        <FaStepForward
+          size={50}
+          color="#FFF"
+          onClick={handleNext}
+          style={{ marginTop: 5, marginBottom: 5 }}
+        />
+      </div>
     </div>
   );
 };
+
+// Timer countdown
+// time will be our prop input but I have no idea if this code works as it's a mix of solutions found in google
+const Timer = ({ time }) => {
+  const [remainingTime, setRemainingTime] = useState(time);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime % 60;
+
+  return (
+    <div
+      className=" mx-auto mt-7 rounded-md bg-[rgb(187,220,163,0.6)] p-10 text-center"
+      style={{ width: '50%', fontSize: '2.3rem' }}
+    >
+      {minutes.toString().padStart(2, '0')}:
+      {seconds.toString().padStart(2, '0')}
+    </div>
+  );
+};
+
+// User's notes
+function NoteForm(props) {
+  const [noteText, setNoteText] = useState('');
+
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit(noteText);
+    setNoteText('');
+  };
+
+  return (
+    <div
+      className=" mx-auto mt-7 rounded-md bg-[rgb(187,220,163,0.6)] p-7 text-center"
+      style={{ width: '50%', fontSize: '1.5rem' }}
+    >
+      <form onSubmit={handleSubmit}>
+        <textarea
+          className="rounded-md bg-[rgb(211,211,211,0.8)]"
+          placeholder="Enter your note here..."
+          value={noteText}
+          onChange={handleChange}
+        />
+        <button
+          className="rounded-md bg-[rgb(187,220,163,0.6)] p-2"
+          type="submit"
+        >
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
+}
 
 const MainBody = () => {
   return (
@@ -103,6 +179,8 @@ const MainBody = () => {
         <Navbar />
         <TextBox />
         <MusicPlayer />
+        <Timer />
+        <NoteForm />
       </div>
     </>
   );
