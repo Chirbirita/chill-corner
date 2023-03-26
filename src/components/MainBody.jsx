58187c9e7dced02afe6eec46ab8bd2cd594dc051
dineshthapa2000@gmail.com <dineshthapa2000@gmail.com>
2023-03-26 10:35:17 +0100

    working with web responsive

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,13 +1,18 @@
-import SelectTheme from "./SelectTheme"
+
+
 const MainBody = () => {
-    return (
-        <div className="glass w-4/6 h-5/6 my-auto rounded-lg">
-            
-            
-
-            
+    
+    
+      return (
+        <>
+        <div className="glass w-4/6 h-5/6 my-auto rounded-lg container">
+          
+          
         </div>
-    )
-
+        </>
+      );
 }
+    
+    
+            
 export default MainBody
--- src/components/themesContainer/Themes.jsx
+++
@@ -1,6 +0,0 @@
-import theme1 from '../themesContainer/themeImage/sad.gif';
-import theme2 from '../themesContainer/themeImage/happy.gif'
-
-
-export const THEMES=[theme1,theme2];
-
