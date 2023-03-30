49cc529b300d4c5c374d4419206971ba7eafc1dd
dineshthapa2000@gmail.com <dineshthapa2000@gmail.com>
2023-03-30 20:15:40 +0100

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Signin from './components/Signin';
-import SideBar from './components/SideBar';
-import MainBody from './components/MainBody';
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,8 +19,6 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <div className='w-full mx-auto overflow-scroll border border-3 flex items-center justify-center h-screen py-3'>
         <Signin />
-        <SideBar/>
-        <MainBody/>
       </div>
     </QueryClientProvider>
   )
